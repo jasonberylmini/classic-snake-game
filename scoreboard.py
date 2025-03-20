0986d18f2eacb8aec31322e83bd2aa2b1e90a0e2
jasonberylmini <jasonberylmini@gmail.com>
2025-03-20 08:25:20 +0530

    add highscore stays the same for next game.

--- scoreboard.py
+++ scoreboard.py
@@ -6,7 +6,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -20,6 +21,8 @@ class Score(Turtle):
     def reset_score(self):
         if self.score > self.highscore:
             self.highscore = self.score
+        with open("data.txt", mode = 'w') as data:
+            data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
 
@@ -31,4 +34,3 @@ class Score(Turtle):
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
-
